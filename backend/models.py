1af9452b95b60c82c27615b1c404e2cc52ece8f7
f33lka <lloydxkir@gmail.com>
2025-10-28 13:54:49 +0300

    создание модели проекта для бд

--- backend/models.py
+++ backend/models.py
@@ -10,3 +10,11 @@ class User(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     defects_assigned = db.relationship("Defect", back_populates="assignee", lazy=True)
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False, unique=True)
+    description = db.Column(db.Text, default="")
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    defects = db.relationship("Defect", back_populates="project", lazy=True, cascade="all, delete-orphan")
